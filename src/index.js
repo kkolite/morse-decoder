661fe598db718b8f75c20a6a20ee7fb9188eb666
kkolite <litavoregor@mail.ru>
2022-07-26 22:52:37 +0300

    create

--- src/index.js
+++ src/index.js
@@ -35,10 +35,43 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = [];
+    let str = '';
+    let litavar = 0
+
+    for (i = 0; i < expr.length; i+=10) {
+        
+        if (expr[i] == '*') {
+            str += '*';
+        }
+        
+        else {
+            for (k = 0; k < 10; k+=2) {
+                if (expr[i+k] == '1' && expr[i+k+1] == '1') {
+                    str += '-';
+                }
+                else if (expr[i+k] == '1' && expr[i+k+1] == '0') {
+                    str += '.';
+                }
+                else { litavar += 1}               
+            }
+        }
+
+        str += ' '
+    }
+     result = str.split(' ');
+
+    let answer ='';
+
+    for (m = 0; m < result.length - 1; m++) {
+        answer += MORSE_TABLE[result[m]];
+    }
+
+    return answer
 }
 
 module.exports = {
